c0eb255f5e86f94d60e790bce44b783f12b7694c
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-10-21 11:18:25 -0300

    Create Game class

--- gatito/source/level.py
+++ gatito/source/level.py
@@ -3,8 +3,14 @@ from settings import *
 from tile import Tile
 from player import Player
 
+
 class Level:
-    def __init__(self) -> None:
+    def __init__(self, level_map: list[str] = LEVEL_MAP) -> None:
+        """Create and load the entire game (level)
+
+        Args:
+            level_map (list[str], optional): Matrix of tile and entities. Defaults to LEVEL_MAP.
+        """
 
         # level setup
         self.display_surface = pygame.display.get_surface()
@@ -13,23 +19,36 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.active_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
-        
-        self.setup_level()
-    
-    
-    def setup_level(self):
-        for row_index, row in enumerate(LEVEL_MAP):
+
+        self.setup_level(level_map)
+
+    def setup_level(self, level_map: list[str]) -> None:
+        """Load the level map
+
+        Args:
+            level_map (list[str]): An matrix with the initial position of tiles and entities
+        """
+
+        for row_index, row in enumerate(level_map):
             y = row_index * TILE_SIZE
             for column_index, column in enumerate(row):
                 x = column_index * TILE_SIZE
-                
+
                 match column:
                     case 'P':
-                        Player((x,y), self.visible_sprites, self.active_sprites, colliders=self.collision_sprites)
+                        Player((x, y),
+                               self.visible_sprites, self.active_sprites,
+                               colliders=self.collision_sprites)
                     case 'X':
-                        Tile((x,y), self.visible_sprites, self.collision_sprites)
+                        Tile((x, y), self.visible_sprites,
+                             self.collision_sprites)
 
     def run(self):
-        # run the entire game (level)
+        """Run the entire game (level).
+        """
+
+        # Game logic
         self.active_sprites.update()
+
+        # Game graphics
         self.visible_sprites.draw(self.display_surface)
--- gatito/source/main.py
+++ gatito/source/main.py
@@ -3,25 +3,37 @@ import sys
 from settings import *
 from level import Level
 
-# Pygame setup
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Gatito')
-clock = pygame.time.Clock()
+
+class Game:
+    def __init__(self) -> None:
+        """Generate and setup the game settings: Display, level, clock...
+        """
+        # Pygame setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Gatito')
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+
+    def run(self):
+        """Start the game loop, run the level and update the display.
+        """
+
+        # event loop
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill(BG_COLOR)
+            self.level.run()
+
+            # drawing logic
+            pygame.display.update()
+            self.clock.tick(60)
+
 
 if __name__ == '__main__':
-    level = Level()
-
-    # event loop
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        screen.fill(BG_COLOR)
-        level.run()
-
-        # drawing logic
-        pygame.display.update()
-        clock.tick(60)
+    game = Game()
+    game.run()
