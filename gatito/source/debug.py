3cc9dc5f9e87c43d41cdfa33260e461b6c34e250
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-10-28 15:11:39 -0300

    Support tools

---
+++ gatito/source/debug.py
@@ -0,0 +1,24 @@
+import pygame
+pygame.init()
+
+# debug settings
+DEBUG_SIZE = 16
+font = pygame.font.SysFont('FiraCode', DEBUG_SIZE)
+
+
+def debug(*info: any, y=1, x=1) -> None:
+    # get the screen to draw
+    display_surface = pygame.display.get_surface()
+
+    # renderize the text
+    debug_surface = font.render(' '.join(map(str, info)), True, 'White')
+
+    # set the position
+    debug_rect = debug_surface.get_rect(
+        topleft=(x * DEBUG_SIZE, y * DEBUG_SIZE))
+
+    # draw a black bg
+    pygame.draw.rect(display_surface, 'Black', debug_rect)
+
+    # draw the text
+    display_surface.blit(debug_surface, debug_rect)
---
+++ gatito/source/tools.py
@@ -0,0 +1,7 @@
+import pygame
+from os import walk
+from os.path import join
+
+
+def load_assets():
+    ...
