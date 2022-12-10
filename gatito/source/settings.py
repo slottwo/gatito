adbff7c5a2f0e602a783572f22841e88b68b3bf8
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-12-09 23:11:27 -0300

    Update settings.py

--- gatito/source/settings.py
+++ gatito/source/settings.py
@@ -1,20 +1,4 @@
 FPS = 60
 TILE_SIZE = 16
-SCREEN_WIDTH = 320
-SCREEN_HEIGHT = 320
-
-TILE_MAP =  [
-    '                            ',
-    '                            ',
-    '                            ',
-    '                            ',
-    '                            ',
-    '       XXXX           XX    ',
-    '   P                        ',
-    '  LXR         LXR        XX ',
-    ' XXXX                       ',
-    ' XX    X  XXXX              ',
-    '       X  XXXX    XX  XXX   ',
-    '    XXXX  XXXXXX  XX  XXXX  ',
-    'XXXXXXXX  XXXXXXXXXX  XXXX  ',
-    'XXXXXXXX  XXXXXXXXXX  XXXX  ']
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 640
