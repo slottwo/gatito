1bf462418e76d950969f5eb29c7f5bb734dda9b5
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-12-12 00:40:36 -0300

    Load maps with json

--- gatito/source/level.py
+++ gatito/source/level.py
@@ -31,76 +31,8 @@ class Level:
         Args:
             level_map (list[str]):  Matrix of tiles and entities on in their respective positions.
         """
-        
-        level_map = loadmap(level_id)
-        
-        """
-        for line_row, row in enumerate(level_id):
-            y = line_row * TILE_SIZE
-            for column_index, cell in enumerate(row):
-                x = column_index * TILE_SIZE
 
-                match cell:
-                    case 0:
-                        ...
-                    case 1:
-                        Tile('water/deep', (x, y), self.visible_sprites)
-                    case 2:
-                        Tile('water/full', (x, y), self.visible_sprites)
-                    case 3:
-                        Tile('water/bubbles', (x, y), self.visible_sprites)
-                    case 4:
-                        Tile('water/top', (x, y), self.visible_sprites)
-                    case 5:
-                        Tile('rock/bot_air', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 6:
-                        Tile('rock/bot_left_air', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 7:
-                        Tile('rock/bot_right_air', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 8:
-                        Tile('rock/bot', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 9:
-                        Tile('rock/bot_left', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 10:
-                        Tile('rock/bot_right', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 11:
-                        Tile('rock/full', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 12:
-                        Tile('rock/left', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 13:
-                        Tile('rock/bot_right', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 14:
-                        Tile('grass/top', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 15:
-                        Tile('grass/left', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 16:
-                        Tile('grass/right', (x, y), self.visible_sprites,
-                             self.collision_sprites)
-                    case 17:
-                        Tile('cloud/1', (x, y), self.visible_sprites)
-                    case 18:
-                        Tile('cloud/2', (x, y), self.visible_sprites)
-                    case 19:
-                        Tile('cloud/3', (x, y), self.visible_sprites)
-                    case 20:
-                        Tile('cloud/4', (x, y), self.visible_sprites)
-                    case 21:
-                        Item('fish', (x, y), 8, self.visible_sprites,
-                             self.active_sprites)
-                    case 22:
-                        Player((x, y), self.visible_sprites,
-                                self.active_sprites)"""
+        level_map = loadmap(level_id)
 
     def run(self):
 
--- gatito/source/tools.py
+++ gatito/source/tools.py
@@ -1,2 +1,11 @@
-def loadmap(map_id) -> dict:
-    ...
+from json import load
+
+
+def loadmap(map_id) -> list[list]:
+    try:
+        with open(f"gatito/assets/tilemaps/{map_id}.json") as f:
+            j = load(f)
+    except FileNotFoundError:
+        print("Level not found.")
+        exit(1)
+    return [l['data'] for l in j['layers']]
