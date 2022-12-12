6ea783b1a4d70ae78c75d6f75d8d7c8fa7148ee8
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-12-12 00:39:50 -0300

    Titeset guide

--- gatito/source/settings.py
+++ gatito/source/settings.py
@@ -2,3 +2,31 @@ FPS = 60
 TILE_SIZE = 16
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 640
+
+TILE_DICT = {
+    0: ...,
+    1: 'grass/left',
+    2: 'grass/top',
+    3: 'grass/right',
+    4: ...,
+    5: 'water/top',
+    6: ...,
+    7: 'rock/left',
+    8: 'rock/full',
+    9: 'rock/right',
+    10: 'water/bubbles_solo',
+    11: 'water/full',
+    12: ...,
+    13: 'rock/bot_left',
+    14: 'rock/bot',
+    15: 'rock/bot_right',
+    16: 'water/bubbles_left',
+    17: 'water/bubbles',
+    18: 'water/bubbles_right',
+    19: 'rock/bot_air',
+    20: 'rock/bot_left_air',
+    21: 'rock/bot_right_air',
+    22: ...,
+    23: 'water/deep',
+    24: ...
+}
